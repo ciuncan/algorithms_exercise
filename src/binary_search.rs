f1a1acbd3986c989ad269509db2d636dfa3c6119
ciuncan <ceyhuncanu@gmail.com>
2020-08-12 19:55:52 +0300

    Implement binary_search

---
+++ src/binary_search.rs
@@ -0,0 +1,58 @@
+use std::fmt::Debug;
+
+pub fn binary_search<T: Eq + Ord + Debug>(slice: &[T], value: &T) -> Option<usize> {
+    let mut lo = 0;
+    let mut hi = slice.len() - 1;
+
+    while lo <= hi {
+        let mid = (lo + hi) / 2;
+        let focused = &slice[mid];
+
+        if focused == value {
+            return Some(mid);
+        } else if focused < value {
+            lo = mid + 1;
+        } else {
+            hi = mid.wrapping_sub(1);
+        }
+    }
+    None
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::binary_search;
+    use proptest::prelude::*;
+
+    proptest! {
+        #[test]
+        fn find_correct_index_of_existing_item((i, v) in index_and_sorted_vec::<i32>(1000)) {
+            let needle = v[i];
+            assert_eq!(binary_search(&v, &needle), Some(i));
+        }
+
+        #[test]
+        fn find_the_same_result_as_vec_binary_search(v in sorted_vec::<i32>(1000), t in any::<i32>()) {
+            let actual = binary_search(&v, &t);
+            let existing = v.binary_search(&t).ok();
+            assert_eq!(actual, existing);
+        }
+    }
+
+    fn index_and_sorted_vec<T: Arbitrary + Clone + Ord>(max_size: usize) -> impl Strategy<Value = (usize, Vec<T>)> {
+        (1usize..max_size)
+            .prop_flat_map(|size| (0..size)
+                .prop_flat_map(move |index| sorted_vec::<T>(size)
+                    .prop_map(move |vec| (index, vec))))
+    }
+
+    fn sorted_vec<T: Arbitrary + Clone + Ord>(size: usize) -> impl Strategy<Value = Vec<T>> {
+        proptest::collection::btree_set(any::<T>(), size)
+            .prop_map(|set| set.iter()
+                .cloned()
+                .collect::<Vec<_>>()
+            )
+    }
+
+}
